fd6a47255b0816e435f4b589278183b84a713788
Hồ Hoàng <hovanhoang959@gmail.com>
2026-01-19 13:50:03 +0700

    HoVanHoang

---
+++ helloworld/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace HelloWorld {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Ho Van Hoang - 2415053122319");
+        }
+    }
+}
