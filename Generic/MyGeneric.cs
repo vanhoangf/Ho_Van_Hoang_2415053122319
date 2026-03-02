655dcb9e4b8c909b8e579d8879e6873c53012d36
Hồ Hoàng <hovanhoang959@gmail.com>
2026-03-02 17:48:52 +0700

    Nop BT Generic

---
+++ Generic/MyGeneric.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+		public static void display(T() inputArray)
+		{
+			foreach (T element in inputArray)
+				Console.Write(element+ " ");
+		}
+
+		public static T[] Sort(T[] inputArray)
+		{
+		return inputArray;
+		}
+	}
---
+++ Generic/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic_CS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyGenericClass<int> element = new MyGenericClass<int>();
+
+            element.display();
+        }
+    }
+}
