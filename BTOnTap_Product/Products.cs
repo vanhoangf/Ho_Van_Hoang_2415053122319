7fd31c02ed19cedc603542caefbc5b95b7ae310a
Hồ Hoàng <hovanhoang959@gmail.com>
2026-03-16 14:04:11 +0700

    Nop BT

---
+++ BTOnTap_Product/Products.cs
@@ -0,0 +1,26 @@
+﻿namespace BTONTAP
+{
+    internal class Products
+    {
+        public string Product {  get; set; }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Category {  get; set; }
+
+        public Products() { }
+        public Products(string product, int id, string name, double price, string category)
+        {
+            this.Product = product;
+            this.Id = id;
+            this.Name = name;
+            this.Price = price;
+            this.Category = category;
+        }
+
+        public override string ToString()
+        {
+            return $"Product: {this.Product}\nId: {this.Id}\nName: {this.Name}\nPrice: {this.Price}\nCategory: {this.Category}\n";
+        }
+    }
+}
---
+++ BTOnTap_Product/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BTONTAP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Products> Pr = new List<Products>();
+            
+            for (int i = 1; i <= 6; i++)
+            {
+                Products p = new Products();
+                p.Product = "Pr" + i;
+                p.Id = i;
+                p.Name = "SP" + i;
+                p.Price = 100 * i;
+                p.Category = "Loai" + i;
+                Pr.Add(p);
+            }
+
+            Console.WriteLine("San pham gia > 500: ");
+            var SP = Pr.Where(x => x.Price > 500);
+            foreach (Products p in SP) 
+                Console.WriteLine(p);
+            Console.WriteLine("Sap xep tang dan: ");
+            var SX = Pr.OrderBy(x => x.Price).ToList();
+            foreach (Products p in SX)
+                Console.WriteLine(p);
+            Console.WriteLine("Ba san pham re nhat: ");
+            var SPRe = Pr.OrderBy(x => x.Price).Take(3);
+            foreach (Products p in SPRe)
+                Console.WriteLine(p);
+            Console.Write("Nhap ten SP: ");
+            string NhapSP = Console.ReadLine();
+            if (Pr.Any(x => x.Name == NhapSP))
+            {
+                var HThi = Pr.Where(x => x.Name == NhapSP);
+                Console.WriteLine("Tim thay: ");
+                foreach (Products p in HThi)
+                    Console.WriteLine(p);
+            }
+            else
+                Console.WriteLine("Khong tim thay");
+
+            var Tong = Pr.Sum(x => x.Price);
+            Console.Write("Tong gia tri san pham: {0}", Tong);
+            var Tb = Pr.Average(x => x.Price);
+            Console.WriteLine("\nGia trung binh: {0}", Tb);
+            Console.ReadKey();
+        }
+    }
+}
